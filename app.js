d674c0ea6d3545685a70724f61d4d3a3216effc7
davjdo <davido_204@hotmail.com>
2019-04-08 00:10:56 +0200

    testing graphql types

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const { buildSchema } = require('graphql');
 
 const app = express();
 
+const events = [];
+
 // Middleware
 app.use(express.json());
 
@@ -11,12 +13,27 @@ app.use(
 	'/graphql',
 	graphqlHttp({
 		schema: buildSchema(`
+			type Event {
+				_id: ID!
+				title: String!
+				description: String!
+				price: Float!
+				date: String!
+			}
+
+			input EventInput {
+				title: String!
+				description: String!
+				price: Float!
+				date: String!
+			}
+
 			type RootQuery {
-				events: [String!]!
+				events: [Event!]! 
 			}	
 
 			type RootMutation {
-				createEvent(name: String): String
+				createEvent(eventInput: EventInput): Event
 			}
 
 			schema {
@@ -26,11 +43,18 @@ app.use(
 		`),
 		rootValue: {
 			events: () => {
-				return ['Romantic', 'Sailing', 'All-night coding'];
+				return events;
 			},
 			createEvent: args => {
-				const eventName = args.name;
-				return eventName;
+				const event = {
+					_id: Math.random().toString(),
+					title: args.eventInput.title,
+					description: args.eventInput.description,
+					price: +args.eventInput.price,
+					date: args.eventInput.date
+				};
+				events.push(event);
+				return event;
 			}
 		},
 		graphiql: true
