c79920ddf3f7a8f262fa5f3ca52e43af02401b41
davjdo <davido_204@hotmail.com>
2019-04-11 01:55:18 +0200

    add react signup and login feature and add fixed CORS headers

--- app.js
+++ app.js
@@ -9,6 +9,18 @@ const app = express();
 
 // Middleware
 app.use(express.json());
+
+// CORS fixed headers
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+	if (req.method === 'OPTIONS') {
+		return res.sendStatus(200);
+	}
+	next();
+});
+
 app.use(isAuth);
 
 app.use(
