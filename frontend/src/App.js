94a8c90f43a84c7d1a1f4869eb0a795eae6c4922
davjdo <davido_204@hotmail.com>
2019-04-10 21:07:09 +0200

    add navbar feature

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import AuthPage from './pages/Auth';
 import EventsPage from './pages/Events';
 import BookingsPage from './pages/Bookings';
+import MainNavigation from './components/Navigation/MainNavigation';
 import './App.css';
 
 class App extends Component {
 	render() {
 		return (
 			<Router>
-				<Switch>
-					<Redirect from="/" to="/auth" exact />
-					<Route path="/auth" component={AuthPage} />
-					<Route path="/events" component={EventsPage} />
-					<Route path="/bookings" component={BookingsPage} />
-				</Switch>
+				<Fragment>
+					<MainNavigation />
+					<main className="main-content">
+						<Switch>
+							<Redirect from="/" to="/auth" exact />
+							<Route path="/auth" component={AuthPage} />
+							<Route path="/events" component={EventsPage} />
+							<Route path="/bookings" component={BookingsPage} />
+						</Switch>
+					</main>
+				</Fragment>
 			</Router>
 		);
 	}
