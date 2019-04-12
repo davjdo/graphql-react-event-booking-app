d157336702afb3116dceb64255da9a1b015a0962
davjdo <davido_204@hotmail.com>
2019-04-12 23:10:28 +0200

    add logout and modal features

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,8 +1,51 @@
 import React, { Component } from 'react';
-
+import Modal from '../components/Modal/Modal';
+import Backdrop from '../components/Backdrop/Backdrop';
+import './Events.css';
 class EventsPage extends Component {
+	state = {
+		creating: false
+	};
+
+	startCreateEventHandler = () => {
+		this.setState({ creating: true });
+	};
+
+	modalCancelHandler = () => {
+		this.setState({ creating: false });
+	};
+
+	modalConfirmHandler = () => {
+		this.setState({ creating: false });
+	};
+
 	render() {
-		return <h1>The Events Page</h1>;
+		return (
+			<React.Fragment>
+				{this.state.creating && <Backdrop />}
+				{this.state.creating && (
+					<Modal
+						title="Add Event"
+						canCancel
+						canConfirm
+						onCancel={this.modalCancelHandler}
+						onConfirm={this.modalConfirmHandler}
+					>
+						<p>Modal content</p>
+					</Modal>
+				)}
+				<div className="events-control">
+					<p>Share your own events !</p>
+					<button
+						className="btn"
+						style={{ marginRight: '0' }}
+						onClick={this.startCreateEventHandler}
+					>
+						Create Event
+					</button>
+				</div>
+			</React.Fragment>
+		);
 	}
 }
 
